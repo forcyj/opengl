7b01d77b863f2ac920ff7b15f402f96df13e9a73
cyj <itisforcyj@gmail.com>
2022-06-16 23:54:48 +0800

    uniform

--- OpenglTest/OpenglTest/main.cpp
+++ OpenglTest/OpenglTest/main.cpp
@@ -12,6 +12,8 @@
 #include <GLFW/glfw3.h>
 
 #include <iostream>
+#include <cmath>
+
 //void myDisplay(void) {
 //    glClear(GL_COLOR_BUFFER_BIT);
 //    glRectf(-0.5f, -0.5f, 0.5f, 0.5f);
@@ -50,19 +52,17 @@ void checkError(unsigned int target, unsigned int status) {
 
 const char *vertexShaderSource = "#version 330 core\n"
     "layout (location = 0) in vec3 aPos;\n"
-    "out vec4 vertexColor;\n"
     "void main()\n"
     "{\n"
     "   gl_Position = vec4(aPos, 1.0);\n"
-    "   vertexColor = vec4(0.5, 0.0, 0.0, 1.0);\n"
     "}\0";
 
 const char *fragmentShaderSource = "#version 330 core\n"
 "out vec4 FragColor;\n"
-"in vec4 vertexColor;\n"
+"uniform vec4 ourColor;\n"
 "void main() \n"
 "{\n"
-"    FragColor = vertexColor;\n"
+"    FragColor = ourColor;\n"
 "}\0";
 
 int main(int argc, char *argv[]) {
@@ -154,6 +154,9 @@ int main(int argc, char *argv[]) {
     }
 
 
+    int vertexColorLocation = glGetUniformLocation(shaderProgram, "ourColor");
+
+
     while(!glfwWindowShouldClose(window))
     {
         // input
@@ -166,6 +169,11 @@ int main(int argc, char *argv[]) {
         glClear(GL_COLOR_BUFFER_BIT);
 
         glUseProgram(shaderProgram);
+
+        float timeValue = glfwGetTime();
+        float greenValue = (sin(timeValue) / 2.0f) + 0.5f;
+        glUniform4f(vertexColorLocation, 0.0f, greenValue, 0.0f, 1.0f);
+
         glBindVertexArray(VAO);
         glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
         glBindVertexArray(0);
