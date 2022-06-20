1a1d7320d49fba08cf6a67d226dbd48b9e23bce8
caiyongjian <caiyongjian@zuoyebang.com>
2022-06-21 00:58:23 +0800

    mv glsl to shader

--- OpenglTest/OpenglTest/main.cpp
+++ OpenglTest/OpenglTest/main.cpp
@@ -13,6 +13,7 @@
 
 #include <iostream>
 #include <cmath>
+#include "Shader.hpp"
 
 //void myDisplay(void) {
 //    glClear(GL_COLOR_BUFFER_BIT);
@@ -50,23 +51,6 @@ void checkError(unsigned int target, unsigned int status) {
     }
 }
 
-const char *vertexShaderSource = "#version 330 core\n"
-    "layout (location = 0) in vec3 aPos;\n"
-    "layout (location = 1) in vec3 aColor;\n"
-    "out vec3 ourColor;\n"
-    "void main()\n"
-    "{\n"
-    "   gl_Position = vec4(aPos, 1.0);\n"
-    "   ourColor = aColor;\n"
-    "}\0";
-
-const char *fragmentShaderSource = "#version 330 core\n"
-"out vec4 FragColor;\n"
-"in vec3 ourColor;\n"
-"void main() \n"
-"{\n"
-"    FragColor = vec4(ourColor, 1.0);\n"
-"}\0";
 
 int main(int argc, char *argv[]) {
     glfwInit();
@@ -92,30 +76,9 @@ int main(int argc, char *argv[]) {
     }
 
     // preprare
+    Shader shader("vertex.glsl", "fragment.glsl");
 
-    unsigned int vertexShader;
-    vertexShader = glCreateShader(GL_VERTEX_SHADER);
-    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
-    glCompileShader(vertexShader);
-    checkError(vertexShader, GL_COMPILE_STATUS);
-
-    unsigned int fragmentShader;
-    fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
-    glCompileShader(fragmentShader);
-    checkError(fragmentShader, GL_COMPILE_STATUS);
-
-    unsigned int shaderProgram;
-    shaderProgram = glCreateProgram();
-    glAttachShader(shaderProgram, vertexShader);
-    glAttachShader(shaderProgram, fragmentShader);
-    glLinkProgram(shaderProgram);
-    checkError(fragmentShader, GL_LINK_STATUS);
-
-    glUseProgram(shaderProgram);
-    glDeleteShader(vertexShader);
-    glDeleteShader(fragmentShader);
-
+    shader.use();
     // 创建VAO/VBO
     unsigned int VAO;
     glGenVertexArrays(1, &VAO);
@@ -175,7 +138,7 @@ int main(int argc, char *argv[]) {
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
-        glUseProgram(shaderProgram);
+        shader.use();
 
 //        float timeValue = glfwGetTime();
 //        float greenValue = (sin(timeValue) / 2.0f) + 0.5f;
