e3f2070745a4fcd8822f59749448356160590ab3
caiyongjian <caiyongjian@zuoyebang.com>
2022-09-10 00:29:09 +0800

    Materials.3

--- OpenglTest/OpenglTest/main.cpp
+++ OpenglTest/OpenglTest/main.cpp
@@ -274,8 +274,17 @@ int main(int argc, char *argv[]) {
         shader.setVec3("material.diffuse",  1.0f, 0.5f, 0.31f);
         shader.setVec3("material.specular", 0.5f, 0.5f, 0.5f);
         shader.setFloat("material.shininess", 32.0f);
-        shader.setVec3("light.ambient",  0.2f, 0.2f, 0.2f);
-        shader.setVec3("light.diffuse",  0.5f, 0.5f, 0.5f); // 将光照调暗了一些以搭配场景
+        
+        glm::vec3 lightColor;
+        lightColor.x = sin(glfwGetTime() * 2.0f);
+        lightColor.y = sin(glfwGetTime() * 0.7f);
+        lightColor.z = sin(glfwGetTime() * 1.3f);
+
+        glm::vec3 diffuseColor = lightColor   * glm::vec3(0.5f); // 降低影响
+        glm::vec3 ambientColor = diffuseColor * glm::vec3(0.2f); // 很低的影响
+        
+        shader.setVec3("light.ambient",  ambientColor);
+        shader.setVec3("light.diffuse", diffuseColor); // 将光照调暗了一些以搭配场景
         shader.setVec3("light.specular", 1.0f, 1.0f, 1.0f);
            
             glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), (GLfloat)WIDTH/(GLfloat)HEIGHT, 0.1f, 100.0f);
