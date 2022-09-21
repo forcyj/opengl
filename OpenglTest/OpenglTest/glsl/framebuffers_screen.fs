fa7e46ac01c2292f8101454f4b07cf8ecbedaafd
caiyongjian <caiyongjian@zuoyebang.com>
2022-09-22 00:16:25 +0800

    framebuffers.3

--- OpenglTest/OpenglTest/glsl/framebuffers_screen.fs
+++ OpenglTest/OpenglTest/glsl/framebuffers_screen.fs
@@ -9,5 +9,13 @@ void main()
 {
 //    vec3 col = texture(screenTexture, TexCoords).rgb;
 //    FragColor = vec4(col, 1.0);
-    FragColor = vec4(vec3(1.0 - texture(screenTexture, TexCoords)), 1.0);
+//    FragColor = vec4(vec3(1.0 - texture(screenTexture, TexCoords)), 1.0);
+    
+//    vec4 col = texture(screenTexture, TexCoords);
+//        float average = (col.r + col.g + col.b) / 3.0;
+//        FragColor = vec4(average, average, average, 1.0);
+    
+    FragColor = texture(screenTexture, TexCoords);
+        float average = 0.2126 * FragColor.r + 0.7152 * FragColor.g + 0.0722 * FragColor.b;
+        FragColor = vec4(average, average, average, 1.0);
 }
