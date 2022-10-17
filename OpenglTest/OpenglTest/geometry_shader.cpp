694a1e466b110134cb72078e2cca07fe605708a2
caiyongjian <caiyongjian@zuoyebang.com>
2022-10-17 23:08:12 +0800

    geometry.2

--- OpenglTest/OpenglTest/geometry_shader.cpp
+++ OpenglTest/OpenglTest/geometry_shader.cpp
@@ -46,10 +46,10 @@ unsigned int SCR_HEIGHT = 600;
 
 
 float points[] = {
-    -0.5f,  0.5f, // 左上
-     0.5f,  0.5f, // 右上
-     0.5f, -0.5f, // 右下
-    -0.5f, -0.5f  // 左下
+    -0.5f,  0.5f, 1.0f, 0.0f, 0.0f, // 左上
+     0.5f,  0.5f, 0.0f, 1.0f, 0.0f, // 右上
+     0.5f, -0.5f, 0.0f, 0.0f, 1.0f, // 右下
+    -0.5f, -0.5f, 1.0f, 1.0f, 0.0f  // 左下
 };
 
 glm::vec3 cameraPos   = glm::vec3(0.0f, 0.0f,  3.0f);
@@ -133,8 +133,10 @@ int geometry_shader_entry() {
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(points), &points, GL_STATIC_DRAW);
     glEnableVertexAttribArray(0);
-    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), (void*)0);
-
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(1);
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(2 * sizeof(float)));
+    
     while(!glfwWindowShouldClose(window))
     {
         // input
